1eca30d8776bd736722bda83fcd1f5cfcbbc42f7
YasseenH <yasseenhilal@utexas.edu>
2025-04-17 20:17:59 +0100

    added dmseg grabbing to the grab log functionality and updated Help Page

--- saved_scripts/save_log.sh
+++ saved_scripts/save_log.sh
@@ -2,23 +2,35 @@
 
 SOURCE="pi@192.168.4.2:/home/pi/printer_data/logs/klippy.log"
 DEST_DIR="/home/pi/saved_logs"
-LOG_FILE_NAME="klippy_$(date +"%T").log"
-DEST_FILE="$DEST_DIR/$LOG_FILE_NAME"
+LOG_TIME=$(date +"%T")
+KLIPPY_LOG_FILE="klippy_${LOG_TIME}.log"
+DMESG_LOG_FILE="dmesg_${LOG_TIME}.log"
+KLIPPY_DEST="$DEST_DIR/$KLIPPY_LOG_FILE"
+DMESG_DEST="$DEST_DIR/$DMESG_LOG_FILE"
 JSON_FILE="/etc/OliveTin/entities/logs.json"
 
-# Try with password A
-sshpass -p "raspberry" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$SOURCE" "$DEST_FILE"
+#klippy.log: password A
+sshpass -p "raspberry" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$SOURCE" "$KLIPPY_DEST"
 
-if [ $? -eq 0 ]; then
-    echo "Success"
-else
-    # Try with password B
-    sshpass -p "Smcc13JLJpA22Jp" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$SOURCE" "$DEST_FILE"
-    
-    if [ $? -eq 0 ]; then
-        echo "Success"
-    else
-        echo "Failed to copy log from printer."
+if [ $? -ne 0 ]; then
+    # klippy.log: password B
+    sshpass -p "Smcc13JLJpA22Jp" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$SOURCE" "$KLIPPY_DEST"
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to copy klippy log from printer."
+        exit 1
+    fi
+fi
+
+#dmseg.log: password A
+sshpass -p "raspberry" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null pi@192.168.4.2 "dmesg" > "$DMESG_DEST"
+
+if [ $? -ne 0 ]; then
+    # dmesg.log: password B
+    sshpass -p "Smcc13JLJpA22Jp" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null pi@192.168.4.2 "dmesg" > "$DMESG_DEST"
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to capture dmesg log from printer."
         exit 1
     fi
 fi
@@ -27,4 +39,5 @@ if [ ! -f "$JSON_FILE" ]; then
     touch "$JSON_FILE"
 fi
 
-echo "{\"Name\":\"$LOG_FILE_NAME\"}" >> "$JSON_FILE"
+echo "{\"Name\":\"$KLIPPY_LOG_FILE\"}" >> "$JSON_FILE"
+echo "{\"Name\":\"$DMESG_LOG_FILE\"}" >> "$JSON_FILE"
